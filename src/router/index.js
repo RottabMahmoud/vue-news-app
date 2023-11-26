00dbe6ca1437c12da5101a04f5cf750f649b7443
Rottab <totundberuhmt@gmail.com>
2023-11-26 12:52:59 +0200

    Checkpoint of refurnishing.

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import NewsView from "../views/NewsView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/newsview",
+    name: "newsView",
+    component: NewsView,
   },
   {
     path: "/about",
